7dd95efa7cebeae667d4cfd31d0004a83d4d9985
Manuel Castro <mhcd1984@gmail.com>
2022-11-17 22:58:09 -0500

    Add profile view controller

---
+++ simpleStock/simpleStock/ProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileViewController.swift
+//  simpleStock
+//
+//  Created by manuel  castro  on 11/17/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBAction func backButton(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
