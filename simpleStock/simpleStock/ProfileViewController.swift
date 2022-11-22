6680ab6394c17045d43a7291e879315476a802b0
Velo23 <64806129+Velo23@users.noreply.github.com>
2022-11-22 02:54:19 -0500

    Modify UserProfile

--- simpleStock/simpleStock/ProfileViewController.swift
+++ simpleStock/simpleStock/ProfileViewController.swift
@@ -6,9 +6,22 @@
 //
 
 import UIKit
+import Parse
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var firstNameField: UITextField!
+    
+    @IBOutlet weak var lastNameField: UITextField!
+    
+    
+    @IBOutlet weak var emailField: UITextField!
+    
+    
+    @IBOutlet weak var phoneField: UITextField!
+    
+    
+    
     @IBAction func backButton(_ sender: Any) {
         
     }
@@ -19,6 +32,26 @@ class ProfileViewController: UIViewController {
     }
     
 
+    @IBAction func onSave(_ sender: Any) {
+    let user = PFUser()
+        user.username = firstNameField.text
+        user.username = lastNameField.text
+        user.email = emailField.text
+
+            // other fields can be set just like with PFObject
+        user["phone"] = phoneField.text
+        
+        //user.saveInBackground { success, error in
+          // if success {
+                
+               // performSegue(withIdentifier: nil, sender: nil)
+               //  @Evel Honore: No segue being performed as of now but let me knoe where does this transiton to
+            //}
+           
+      //  }
+        
+        
+    }
     /*
     // MARK: - Navigation
 
