290968583ddcfec7c182ccfe0e35487063368f0c
manuel castro <99112830+manuelcastroFau@users.noreply.github.com>
2022-12-12 23:42:51 -0500

    Implement ChartViewController and allow to share to multiples controllers (#65)
    Co-authored-by: Manuel Castro <mhcd1984@gmail.com>

---
+++ simpleStock/simpleStock/ChartsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ChartsViewController.swift
+//  simpleStock
+//
+//  Created by manuel  castro  on 12/12/22.
+//
+
+import UIKit
+import Parse
+
+class ChartsViewController: UIViewController {
+    
+    var stock: PFObject!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
